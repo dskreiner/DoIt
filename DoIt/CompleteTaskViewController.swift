4cc6667a905fdf4c3f3748b3c41d45c9d9e244a3
Scott Kreiner <scott@kreiners.net>
2017-02-12 19:24:01 -0700

    Complete with CoreData

--- DoIt/CompleteTaskViewController.swift
+++ DoIt/CompleteTaskViewController.swift
@@ -10,25 +10,25 @@ import UIKit
 
 class CompleteTaskViewController: UIViewController {
 
-    var task = Task()
+    var task : Task? = nil
     
     @IBOutlet weak var taskLabel: UILabel!
 
-    var previousVC = TasksViewController()
-    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if task.important {
-            taskLabel.text = "❗️\(task.name)"
+        if task!.important {
+            taskLabel.text = "❗️\(task!.name!)"
         } else {
-            taskLabel.text = task.name
+            taskLabel.text = task!.name!
         }
     }
 
     @IBAction func completeTapped(_ sender: Any) {
-        previousVC.tasks.remove(at: previousVC.selectedIndex)
-        previousVC.tableView.reloadData()
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(task!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
         navigationController!.popViewController(animated: true)
     }
 
